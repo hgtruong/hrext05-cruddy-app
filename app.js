87a08830e58cf95c4ca0cb2af76fdfee6a709332
hgtruong <huygtruong@gmail.com>
2018-10-31 16:39:32 -0700

    Added individually remove functionality

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
   console.log('before\n', window.localStorage);
   var todoCounter = 0;
 
+
   renderDisplay(localStorage);
   // add event listener
   $('.addTextBtn').on('click', function(){
@@ -18,13 +19,20 @@ $(document).ready(function(){
     var curTextValue = $('#theKey').val(); // reading from <input>
     var curKeyValue = 'theKey' + todoCounter;
     todoCounter++;
-    localStorage.setItem(curKeyValue, curTextValue);
-    renderDisplay(localStorage);
 
-/**/
+    var allValues = Object.values(localStorage);
+    if(allValues.indexOf(curTextValue) === -1 && curTextValue.length > 0){
+      localStorage.setItem(curKeyValue, curTextValue);
+    } else if (curTextValue.length === 0) {
+      alert('Entry can\'t be blank');
+    } else {
+      alert('Similar entry found');
+    }
+
+
+    renderDisplay(localStorage);
   });
 
-  // remove item from app
 
   // listen for click event (del)
   $('.clearCacheBtn').on('click', function(){
@@ -36,18 +44,24 @@ $(document).ready(function(){
 
   /// ***** renderDisplay(jsonObject); **********
   function renderDisplay(localStorage){
-    console.log(localStorage);
     $('.showText').append('<div id=accordion> </div>');
+
     for(var key in localStorage){
       if(localStorage.hasOwnProperty(key)){
         var header = $('<h3></h3>');
         var counter = key.charAt(key.length-1);
-        var todoContent = $('<div></div>');
-        header.html(`${++counter}`);
+        ++counter; // since we start at 0
+        var todoContent = $(`<div id=content${counter}></div>`);
+        var removeOption = $('<div class=remove> remove </div>');
+
+        header.html(`${counter}`);
         todoContent.html(`<p> ${localStorage[key]} </p>`)
         $('#accordion').append(header,todoContent);
+        console.log(`#content${counter}`)
+        $(`#content${counter}`).append(removeOption);
       }
     }
+
     $('#accordion').accordion({
       collapsible: true
     });
@@ -57,9 +71,19 @@ $(document).ready(function(){
   $(function() {
     $('.addTextBtn').button({icon:'ui-icon-plus'});
     $('.clearCacheBtn').button({icon:'ui-icon-close'});
-    // $('#accordion').accordion({
-    //   // collapsible: true;
-    // });
   });
+
+  // Remove accordion
+  $(document).on('click', '.remove', function(e){
+     var lsLocation = $(this).parent()[0].id;
+    console.log(typeof lsLocation);
+    var deletedLocation =  Number(lsLocation.charAt(lsLocation.length - 1));
+    var deletedKey = 'theKey' + (--deletedLocation);
+    console.log(deletedKey);
+    localStorage.removeItem(deletedKey);
+    $(this).parent('div').prev('h3').andSelf().remove();
+    console.log('storageAfter', localStorage);
+  });
+
 
 });
