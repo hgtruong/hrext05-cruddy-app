3b845f7b8b83c54c248bfd25796938090cc328e2
hgtruong <huygtruong@gmail.com>
2018-10-31 17:24:13 -0700

    Added edit feature

--- app.js
+++ app.js
@@ -44,21 +44,21 @@ $(document).ready(function(){
 
   /// ***** renderDisplay(jsonObject); **********
   function renderDisplay(localStorage){
-    $('.showText').append('<div id=accordion> </div>');
-
+      $('.showText').append('<div id=accordion> </div>');
     for(var key in localStorage){
       if(localStorage.hasOwnProperty(key)){
         var header = $('<h3></h3>');
         var counter = key.charAt(key.length-1);
         ++counter; // since we start at 0
         var todoContent = $(`<div id=content${counter}></div>`);
+        var editContent = $(`<div class=edit> edit </div>`);
         var removeOption = $('<div class=remove> remove </div>');
 
         header.html(`${counter}`);
         todoContent.html(`<p> ${localStorage[key]} </p>`)
         $('#accordion').append(header,todoContent);
         console.log(`#content${counter}`)
-        $(`#content${counter}`).append(removeOption);
+        $(`#content${counter}`).append(editContent, removeOption);
       }
     }
 
@@ -73,17 +73,39 @@ $(document).ready(function(){
     $('.clearCacheBtn').button({icon:'ui-icon-close'});
   });
 
-  // Remove accordion
-  $(document).on('click', '.remove', function(e){
-     var lsLocation = $(this).parent()[0].id;
+  // Remove Todo
+  $(document).on('click', '.remove', function(event){
+    var lsLocation = $(this).parent()[0].id;
     console.log(typeof lsLocation);
     var deletedLocation =  Number(lsLocation.charAt(lsLocation.length - 1));
     var deletedKey = 'theKey' + (--deletedLocation);
     console.log(deletedKey);
     localStorage.removeItem(deletedKey);
     $(this).parent('div').prev('h3').andSelf().remove();
-    console.log('storageAfter', localStorage);
+
+    $('.showText').empty();
+    renderDisplay(localStorage);
+
   });
+
+  // Edit Todo
+  $(document).on('click', '.edit', function(event){
+    var txt;
+    var lsLocation = $(this).parent()[0].id;
+    console.log(typeof lsLocation);
+    var editLocation =  Number(lsLocation.charAt(lsLocation.length - 1));
+    var editKey = 'theKey' + (--editLocation);
+    var editContent = prompt("Please enter your edit:");
+    if (editContent !== null || editContent !== "") {
+        localStorage.setItem(editKey, editContent);
+    }
+    $('.showText').empty();
+    renderDisplay(localStorage);
+
+
+
+
+  })
 
 
 });
